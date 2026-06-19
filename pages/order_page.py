7468056bf9b081d579fef6fb4a61becc62fca229
Valentina Loskutova <valentina.losktova@gmail.com>
2026-06-19 16:04:57 +0300

    Добавлены Allure-шаги в main_page и order_page

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,6 +10,7 @@ class OrderPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
+    @allure.step("Заполнить форму заказа")
     def fill_order_form(self, data):
         self.wait_for_visibility(OrderPageLocators.NAME_INPUT)
         self.send_keys_to_element(OrderPageLocators.NAME_INPUT, data["name"])
@@ -17,25 +19,30 @@ class OrderPage(BasePage):
         self.select_metro(data["metro"])
         self.send_keys_to_element(OrderPageLocators.PHONE_INPUT, data["phone"])
 
+    @allure.step("Клик по кнопке 'Далее'")
     def click_order_button(self):
         self.click_element(OrderPageLocators.ORDER_BUTTON_NEXT)
 
+    @allure.step("Клик по финальной кнопке 'Заказать'")
     def click_final_order_button(self):
         button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(OrderPageLocators.ORDER_BUTTON_FINAL)
         )
         self.driver.execute_script("arguments[0].click();", button)
 
+    @allure.step("Получить сообщение об успешном заказе")
     def get_success_message(self):
         message = self.wait_for_visibility(OrderPageLocators.SUCCESS_MESSAGE)
         return message.text
 
+    @allure.step("Выбрать станцию метро {metro_name}")
     def select_metro(self, metro_name):
         metro_input = self.find_element(OrderPageLocators.METRO_INPUT)
         metro_input.click()
         metro_input.send_keys(Keys.ARROW_DOWN)
         metro_input.send_keys(Keys.ENTER)
 
+    @allure.step("Заполнить форму аренды")
     def fill_rental_form(self, date, period, color, comment):
         self.wait_for_visibility(OrderPageLocators.DATE_INPUT)
 
@@ -56,8 +63,3 @@ class OrderPage(BasePage):
 
         self.send_keys_to_element(OrderPageLocators.COMMENT_INPUT, comment)
         self.click_element(OrderPageLocators.ORDER_BUTTON_FINAL)
-
-    def confirm_order(self):
-        wait = WebDriverWait(self.driver, 10)
-        confirm_button = wait.until(EC.element_to_be_clickable(OrderPageLocators.CONFIRM_BUTTON))
-        self.driver.execute_script("arguments[0].click();", confirm_button)
